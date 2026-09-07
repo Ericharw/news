5b9c2a18f7b19e5c2e0412384016823eb2d0525b
Ericharw <ericharizki12@gmail.com>
2026-09-07 07:59:55 +0700

    fix: bug

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/news",
+  connectionString: process.env.DATABASE_URL || "postgresql://news_user:Ericha12_@localhost:5432/news",
 });
 
 export default pool;
